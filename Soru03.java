a68088284bd12baaf68f0b10428fe5178125cdfc
aliosmanakca <akcaaliosman@gmail.com>
2021-08-25 16:26:12 -0400

    .

---
+++ Soru03.java
@@ -0,0 +1,23 @@
+package Practises;
+
+import java.util.Scanner;
+
+public class Soru03 {
+
+	public static void main(String[] args) {
+		/*
+		 *  Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+		 *  formula
+		    
+		    c = (f-32)*5/9
+		 */
+		Scanner scan=new Scanner (System.in);
+		System.out.println("Fahrenheit degerini giriniz");
+		double f=scan.nextDouble();
+		double c=(f-32)*5/9;
+		
+		System.out.println("celcius degeri : "+c);
+scan.close();
+	}
+
+}
